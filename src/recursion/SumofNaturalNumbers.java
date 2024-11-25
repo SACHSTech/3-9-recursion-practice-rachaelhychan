f38151cbd7438c565ac9e4b5fa486c91b450dcdf
rachaelhychan <rachael.chan25@ycdsbk12.ca>
2024-11-25 10:24:34 -0500

    Completed SumofNaturalNumbers

--- src/recursion/SumofNaturalNumbers.java
+++ src/recursion/SumofNaturalNumbers.java
@@ -1,8 +1,16 @@
 package recursion;
 
 public class SumofNaturalNumbers {
+    static int theSum = 0;
     public static int sum(int n) {
-        return 0;
+        if(n < 1){
+            return theSum;
+        }else{
+            theSum = theSum + n;
+            sum(n - 1);
+        }
+
+        return theSum;
     }
 
     public static void main(String[] args) {
