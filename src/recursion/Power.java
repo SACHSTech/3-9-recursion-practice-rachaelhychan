88030aacfc381f790bb7794db6754fe0da2f32e0
rachaelhychan <rachael.chan25@ycdsbk12.ca>
2024-11-25 11:40:35 -0500

    Completed Power

---
+++ src/recursion/Power.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class Power {
+    public static int power(int base, int exponent){
+        if(exponent == 0){
+            return 1;
+        }else{
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 4));
+    }
+}
